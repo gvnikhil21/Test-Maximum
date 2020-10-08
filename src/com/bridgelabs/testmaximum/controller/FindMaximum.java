ab74db2544977fb8b5999e3d042b803273110d16
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-08 18:44:59 +0530

    Added: Welcome message added

---
+++ src/com/bridgelabs/testmaximum/controller/FindMaximum.java
@@ -0,0 +1,9 @@
+package com.bridgelabs.testmaximum.controller;
+
+public class FindMaximum {
+
+	public static void main(String[] args) {
+		// welcome message
+		System.out.println("Welcome to Find Maximum");
+	}
+}
