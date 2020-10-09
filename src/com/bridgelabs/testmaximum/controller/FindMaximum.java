bb32c4fde8c0be7ace9a4f0785e2d7149b870dcb
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-09 11:01:58 +0530

    Added: Given float numbers find maximum functionality added

--- src/com/bridgelabs/testmaximum/controller/FindMaximum.java
+++ src/com/bridgelabs/testmaximum/controller/FindMaximum.java
@@ -6,11 +6,17 @@ public class FindMaximum {
 		// welcome message
 		System.out.println("Welcome to Find Maximum");
 
-		Integer x = 10;
-		Integer y = 5;
-		Integer z = 8;
+		// find max Integer
+		Integer xInt = 10;
+		Integer yInt = 5;
+		Integer zInt = 8;
+		findMaxInteger(xInt, yInt, zInt);
 
-		findMaxInteger(x, y, z);
+		// find max Float
+		Float xFloat = 10.5f;
+		Float yFloat = 5.5f;
+		Float zFloat = 8.5f;
+		findMaxFloat(xFloat, yFloat, zFloat);
 	}
 
 	public static Integer findMaxInteger(Integer x, Integer y, Integer z) {
@@ -23,7 +29,21 @@ public class FindMaximum {
 		return max;
 	}
 
+	public static Float findMaxFloat(Float x, Float y, Float z) {
+		Float max = x;
+		if (y.compareTo(max) > 0)
+			max = y;
+		if (z.compareTo(max) > 0)
+			max = z;
+		printMaxFloat(max, x, y, z);
+		return max;
+	}
+
 	private static void printMaxInteger(Integer max, Integer x, Integer y, Integer z) {
-		System.out.println("Maximum of " + x + ", " + y + ", " + z + " is : " + max);
+		System.out.println("Maximum Integer of " + x + ", " + y + ", " + z + " is : " + max);
+	}
+
+	private static void printMaxFloat(Float max, Float x, Float y, Float z) {
+		System.out.println("Maximum Float of " + x + ", " + y + ", " + z + " is : " + max);
 	}
 }
