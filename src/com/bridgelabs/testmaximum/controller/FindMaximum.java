374743c9d5c65f3ef1469a691faeeb927ba796c8
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-09 12:10:13 +0530

    Refactored: Code refactored to make on generic method to find maximum instead of three different methods

--- src/com/bridgelabs/testmaximum/controller/FindMaximum.java
+++ src/com/bridgelabs/testmaximum/controller/FindMaximum.java
@@ -10,60 +10,33 @@ public class FindMaximum {
 		Integer xInt = 10;
 		Integer yInt = 5;
 		Integer zInt = 8;
-		findMaxInteger(xInt, yInt, zInt);
+		findMax(xInt, yInt, zInt);
 
 		// find max Float
 		Float xFloat = 10.5f;
 		Float yFloat = 5.5f;
 		Float zFloat = 8.5f;
-		findMaxFloat(xFloat, yFloat, zFloat);
+		findMax(xFloat, yFloat, zFloat);
 
 		// find max String
 		String xString = "Apple";
 		String yString = "Peach";
 		String zString = "Banana";
-		findMaxString(xString, yString, zString);
+		findMax(xString, yString, zString);
 	}
 
-	public static Integer findMaxInteger(Integer x, Integer y, Integer z) {
-		Integer max = x;
+	public static <E extends Comparable<E>> E findMax(E x, E y, E z) {
+		E max = x;
 		if (y.compareTo(max) > 0)
 			max = y;
 		if (z.compareTo(max) > 0)
 			max = z;
-		printMaxInteger(max, x, y, z);
+		printMax(max, x, y, z);
 		return max;
 	}
 
-	public static Float findMaxFloat(Float x, Float y, Float z) {
-		Float max = x;
-		if (y.compareTo(max) > 0)
-			max = y;
-		if (z.compareTo(max) > 0)
-			max = z;
-		printMaxFloat(max, x, y, z);
-		return max;
+	private static <E> void printMax(E max, E x, E y, E z) {
+		System.out.println("Maximum of " + x + ", " + y + ", " + z + " is : " + max);
 	}
 
-	public static String findMaxString(String x, String y, String z) {
-		String max = x;
-		if (y.compareTo(max) > 0)
-			max = y;
-		if (z.compareTo(max) > 0)
-			max = z;
-		printMaxString(max, x, y, z);
-		return max;
-	}
-
-	private static void printMaxInteger(Integer max, Integer x, Integer y, Integer z) {
-		System.out.println("Maximum Integer of " + x + ", " + y + ", " + z + " is : " + max);
-	}
-
-	private static void printMaxFloat(Float max, Float x, Float y, Float z) {
-		System.out.println("Maximum Float of " + x + ", " + y + ", " + z + " is : " + max);
-	}
-
-	private static void printMaxString(String max, String x, String y, String z) {
-		System.out.println("Maximum String of " + x + ", " + y + ", " + z + " is : " + max);
-	}
 }
--- test/com/bridgelabs/testmaximum/tester/FindMaximumTest.java
+++ test/com/bridgelabs/testmaximum/tester/FindMaximumTest.java
@@ -9,55 +9,55 @@ public class FindMaximumTest {
 
 	@Test
 	public void givenMaxInteger_AtFirstPosition_ShouldReturnSameInteger() {
-		Integer max = FindMaximum.findMaxInteger(10, 5, 8);
+		Integer max = FindMaximum.findMax(10, 5, 8);
 		assertEquals(Integer.valueOf(10), max);
 	}
 
 	@Test
 	public void givenMaxInteger_AtSecondPosition_ShouldReturnSameInteger() {
-		Integer max = FindMaximum.findMaxInteger(5, 10, 8);
+		Integer max = FindMaximum.findMax(5, 10, 8);
 		assertEquals(Integer.valueOf(10), max);
 	}
 
 	@Test
 	public void givenMaxInteger_AtThirdPosition_ShouldReturnSameInteger() {
-		Integer max = FindMaximum.findMaxInteger(5, 8, 10);
+		Integer max = FindMaximum.findMax(5, 8, 10);
 		assertEquals(Integer.valueOf(10), max);
 	}
 
 	@Test
 	public void givenMaxFloat_AtFirstPosition_ShouldReturnSameFloat() {
-		Float max = FindMaximum.findMaxFloat(10.5f, 5.5f, 8.5f);
+		Float max = FindMaximum.findMax(10.5f, 5.5f, 8.5f);
 		assertEquals(Float.valueOf(10.5f), max);
 	}
 
 	@Test
 	public void givenMaxFloat_AtSecondPosition_ShouldReturnSameFloat() {
-		Float max = FindMaximum.findMaxFloat(5.5f, 10.5f, 8.5f);
+		Float max = FindMaximum.findMax(5.5f, 10.5f, 8.5f);
 		assertEquals(Float.valueOf(10.5f), max);
 	}
 
 	@Test
 	public void givenMaxFloat_AtThirdPosition_ShouldReturnSameFloat() {
-		Float max = FindMaximum.findMaxFloat(8.5f, 5.5f, 10.5f);
+		Float max = FindMaximum.findMax(8.5f, 5.5f, 10.5f);
 		assertEquals(Float.valueOf(10.5f), max);
 	}
 
 	@Test
 	public void givenMaxString_AtFirstPosition_ShouldReturnSameString() {
-		String max = FindMaximum.findMaxString("Peach", "Apple", "Banana");
+		String max = FindMaximum.findMax("Peach", "Apple", "Banana");
 		assertEquals("Peach", max);
 	}
 
 	@Test
 	public void givenMaxString_AtSecondPosition_ShouldReturnSameString() {
-		String max = FindMaximum.findMaxString("Apple", "Peach", "Banana");
+		String max = FindMaximum.findMax("Apple", "Peach", "Banana");
 		assertEquals("Peach", max);
 	}
 
 	@Test
 	public void givenMaxString_AtThirdPosition_ShouldReturnSameString() {
-		String max = FindMaximum.findMaxString("Apple", "Banana", "Peach");
+		String max = FindMaximum.findMax("Apple", "Banana", "Peach");
 		assertEquals("Peach", max);
 	}
 }
