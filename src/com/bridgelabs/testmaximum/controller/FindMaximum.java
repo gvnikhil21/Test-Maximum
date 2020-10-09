afe02a78914a80070494e07a8f509d884db5f2df
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-09 10:25:08 +0530

    Added: Given integers find maximum functionality added

--- src/com/bridgelabs/testmaximum/controller/FindMaximum.java
+++ src/com/bridgelabs/testmaximum/controller/FindMaximum.java
@@ -5,5 +5,24 @@ public class FindMaximum {
 	public static void main(String[] args) {
 		// welcome message
 		System.out.println("Welcome to Find Maximum");
+
+		Integer x = 10;
+		Integer y = 5;
+		Integer z = 8;
+
+		findMaxInteger(x, y, z);
+	}
+
+	private static void findMaxInteger(Integer x, Integer y, Integer z) {
+		Integer max = x;
+		if (y.compareTo(max) > 0)
+			max = y;
+		if (z.compareTo(max) > 0)
+			max = z;
+		printMaxInteger(max, x, y, z);
+	}
+
+	private static void printMaxInteger(Integer max, Integer x, Integer y, Integer z) {
+		System.out.println("Maximum of " + x + ", " + y + ", " + z + " is : " + max);
 	}
 }
