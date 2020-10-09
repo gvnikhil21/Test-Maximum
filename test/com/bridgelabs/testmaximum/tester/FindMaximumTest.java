be6583b2e605409ed87745a406fde0ea051baf04
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-09 11:08:08 +0530

    Added: Test given max float at first position should return same float functionality added

--- test/com/bridgelabs/testmaximum/tester/FindMaximumTest.java
+++ test/com/bridgelabs/testmaximum/tester/FindMaximumTest.java
@@ -10,18 +10,24 @@ public class FindMaximumTest {
 	@Test
 	public void givenMaxInteger_AtFirstPosition_ShouldReturnSameInteger() {
 		Integer max = FindMaximum.findMaxInteger(10, 5, 8);
-		assertEquals(Integer.valueOf(10), Integer.valueOf(max));
+		assertEquals(Integer.valueOf(10), max);
 	}
 
 	@Test
 	public void givenMaxInteger_AtSecondPosition_ShouldReturnSameInteger() {
 		Integer max = FindMaximum.findMaxInteger(5, 10, 8);
-		assertEquals(Integer.valueOf(10), Integer.valueOf(max));
+		assertEquals(Integer.valueOf(10), max);
 	}
 
 	@Test
 	public void givenMaxInteger_AtThirdPosition_ShouldReturnSameInteger() {
 		Integer max = FindMaximum.findMaxInteger(5, 8, 10);
-		assertEquals(Integer.valueOf(10), Integer.valueOf(max));
+		assertEquals(Integer.valueOf(10), max);
+	}
+
+	@Test
+	public void givenMaxFloat_AtFirstPosition_ShouldReturnSameFloat() {
+		Float max = FindMaximum.findMaxFloat(10.5f, 5.5f, 8.5f);
+		assertEquals(Float.valueOf(10.5f), max);
 	}
 }
